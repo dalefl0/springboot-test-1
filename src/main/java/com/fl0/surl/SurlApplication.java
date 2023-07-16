00fcdd2c6298054e2f5ac9be36bf904bb26060c9
ashutosh-aanand <ashutosh.aanand@gmail.com>
2023-07-17 00:40:07 +0530

    minimal app

--- src/main/java/com/fl0/surl/SurlApplication.java
+++ src/main/java/com/fl0/surl/SurlApplication.java
@@ -2,12 +2,20 @@ package com.fl0.surl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SurlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SurlApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String helloController(){
+		return "Hello from springboot app !!";
 	}
 
 }
